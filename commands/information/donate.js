8c1461aa05d57fd5db08c889e6977d8a2989cbcc
Ramadan Muhammad Azry <azry695@gmail.com>
2025-08-23 12:47:12 +0700

    Update donate.js

--- commands/information/donate.js
+++ commands/information/donate.js
@@ -14,13 +14,8 @@ module.exports = {
                 .replace(/%command%/g, ctx.used.command)
                 .replace(/%footer%/g, config.msg.footer)
                 .replace(/%readmore%/g, config.msg.readmore) :
-                `${formatter.quote("083838039693 (DANA)")}\n` +
-                `${formatter.quote("083838039693 (Pulsa & Kuota)")}\n` +
-                `${formatter.quote("─────")}\n` +
-                `${formatter.quote("https://paypal.me/itsreimau (PayPal)")}\n` +
-                `${formatter.quote("https://saweria.co/itsreimau (Saweria)")}\n` +
-                `${formatter.quote("https://tako.id/itsreimau (Tako)")}\n` +
-                formatter.quote("https://trakteer.id/itsreimau (Trakteer)");
+                `${formatter.quote("083879175089 (gopay)")}\n` +
+                formatter.quote("https://s.id/Unity_Central (Trakteer)");
 
             if (qrisLink) {
                 return await ctx.reply({
